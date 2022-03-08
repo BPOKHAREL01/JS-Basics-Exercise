5d2fdeb5323070b0f3b8fbbd256bf22237777573
“bpokharel01” <bibhasa.pokharel@gmail.com>
2022-03-08 13:23:34 -0500

    Added JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log(3);
+
+/* Fiver main primitives are
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null */
+
+/* Six operative symbols are as follows:
+1. + (addition)
+2. - (subtraction)
+3. * (multiplication)
+4. / (division)
+5. ** (Exponent)
+6. % (Modolus)
+# All of these follow PEMDAS rule */
+
+console.log(5%4);
+console.log(5%4);
+
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+console.log("Hawk" + "eye");
+
+console.log("Bibhasa".length + "Pokharel".length);
+
+console.log("Avengers: Endgame"[16]);
+
+/* BONUS */
+
+console.log("\\");
+
+console.log(5**4-9);
+
+console.log("I".length + "AM".length % "IRON".length - "Man".length);
+
+console.log("CAP");
+console.log("VISION");
+console.log("OKOYE");
+console.log("NICK FURY");
+console.log("GROOT");
+console.log("DOCTOR STRANGER");
+console.log("THOR");
+console.log("STAR-LOGO");
+
+/* IArry = ["CAP", "VISION", "NICK FURY", "GROOT, "DOCTOR STRANGE", "THOR", "STAR-LORD" ]
+console.log(IArrat[0][1] + IArray[1][0] +IArray[2][4] + IArray[3][0] + IArray[4][0] + IArray[6][3] + IArray[7][0]); */
+
+m=25;
+c=4;
+u=m+c;
+console.log(u);
